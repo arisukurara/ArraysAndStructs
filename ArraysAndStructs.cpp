04b2dc2c060a6f4568b3af32fe3031dbcbd3ea57
arisukurara <shawncarter896@gmail.com>
2024-10-18 23:59:12 -0400

    complete

--- ArraysAndStructs.cpp
+++ ArraysAndStructs.cpp
@@ -2,19 +2,74 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+const int NUM_RUNNERS = 99;
+const int NUM_DAYS = 7;
+
+string names[NUM_RUNNERS];
+int runnerData[NUM_RUNNERS][NUM_DAYS];
+
+int getData(string names[], int runnerData[][NUM_DAYS]) {
+    std::ifstream runners("runners.txt");
+    int rowIndex = 0;
+    while (runners >> names[rowIndex]) { // Read the name and puts it into the names array
+        for (int col = 0; col < NUM_DAYS; col++) {
+            runners >> runnerData[rowIndex][col]; // Read miles into milesRan array for each day while on that runner
+            //cout << milesRan[rowIndex][col] << endl;
+        }
+        //cout << names[rowIndex] << endl;
+        rowIndex++; // Move to the next row (next runner)
+    }
+    runners.close();
+    return rowIndex;
+}
+
+double totalMiles(int rowIndex) {
+    double totalMiles = 0;
+    for (int col = 0; col < NUM_DAYS; col++) {
+        totalMiles += runnerData[rowIndex][col];
+    }
+    return totalMiles;
+}
+
+double averageMiles(double totalMiles) {
+    return totalMiles / 7;
+}
+
+void outputResults(int rowIndex) {
+    if (rowIndex == 0) {
+        cout << left << setw(10) << "Name";
+        for (int i = 0; i < NUM_DAYS; i++) {
+            cout << right << setw(10) << ("Day " + to_string(i + 1));
+        }
+        cout << right << setw(10) << "Total";
+        cout << right << setw(10) << "Average";
+        cout << endl;
+        cout << "------------------------------------------------------------------------------------------------------";
+        cout << endl;
+    }
+    cout << left << setw(10) << names[rowIndex]; //prints name for that index
+    for (int col = 0; col < NUM_DAYS; col++) {
+        cout << right << setw(10) << runnerData[rowIndex][col]; // prints data for each day
+    }
+
+    double total = totalMiles(rowIndex);
+
+    cout << right << setw(10) << total;
+    cout << right << setprecision(3) << setw(10) << averageMiles(total);
+    cout << endl;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int numberOfRows = getData(names, runnerData);
+    for (int i = 0; i < numberOfRows; i++) {
+        outputResults(i);
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
